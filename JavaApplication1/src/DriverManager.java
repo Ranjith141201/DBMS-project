7a45985f1a0bf4bc9f190d0bf02a973c09e0e9eb
Ranjith141201 <ranjith.cclub@gmail.com>
2022-09-22 23:58:49 +0530

    first commit

---
+++ JavaApplication1/src/DriverManager.java
@@ -0,0 +1,20 @@
+
+import com.sun.jdi.connect.spi.Connection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 91854
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306bank, String root, String ranjithr2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
